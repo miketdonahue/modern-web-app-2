541e8e62e5c0668ab8e841e23e9013f0aae65211
Michael Donahue <mtdonahue1@gmail.com>
2019-12-01 00:24:56 -0800

    chore: add bundle analyzer

--- next.config.js
+++ next.config.js
@@ -3,80 +3,86 @@ const withLess = require('@zeit/next-less');
 const getCSSModuleLocalIdent = require('react-dev-utils/getCSSModuleLocalIdent');
 const antDesignTheme = require('./styles/components/theme');
 
-module.exports = withCss(
-  withLess({
-    distDir: '.build/client',
-    poweredByHeader: false,
-    cssModules: true,
-    cssLoaderOptions: {
-      getLocalIdent: (context, localIdentName, localName, options) => {
-        // Ensure 3rd-party and global core app (tailwind css) CSS do not get localized
-        if (
-          context.resourcePath.includes('node_modules') ||
-          context.resourcePath.includes('styles/core')
-        ) {
-          return localName;
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+});
+
+module.exports = withBundleAnalyzer(
+  withCss(
+    withLess({
+      distDir: '.build/client',
+      poweredByHeader: false,
+      cssModules: true,
+      cssLoaderOptions: {
+        getLocalIdent: (context, localIdentName, localName, options) => {
+          // Ensure 3rd-party and global core app (tailwind css) CSS do not get localized
+          if (
+            context.resourcePath.includes('node_modules') ||
+            context.resourcePath.includes('styles/core')
+          ) {
+            return localName;
+          }
+
+          return getCSSModuleLocalIdent(
+            context,
+            localIdentName,
+            localName,
+            options
+          );
+        },
+      },
+      lessLoaderOptions: {
+        modifyVars: antDesignTheme,
+        javascriptEnabled: true,
+      },
+      typescript: {
+        ignoreDevErrors: true,
+        ignoreBuildErrors: true,
+      },
+      webpack: (config, { isServer }) => {
+        if (isServer) {
+          const antStyles = /antd\/.*?\/style.*?/;
+          const origExternals = [...config.externals];
+          config.externals = [
+            (context, request, callback) => {
+              if (request.match(antStyles)) return callback();
+              if (typeof origExternals[0] === 'function') {
+                origExternals[0](context, request, callback);
+              } else {
+                callback();
+              }
+            },
+            ...(typeof origExternals[0] === 'function' ? [] : origExternals),
+          ];
+
+          config.module.rules.unshift({
+            test: antStyles,
+            use: 'null-loader',
+          });
         }
 
-        return getCSSModuleLocalIdent(
-          context,
-          localIdentName,
-          localName,
-          options
-        );
-      },
-    },
-    lessLoaderOptions: {
-      modifyVars: antDesignTheme,
-      javascriptEnabled: true,
-    },
-    typescript: {
-      ignoreDevErrors: true,
-      ignoreBuildErrors: true,
-    },
-    webpack: (config, { isServer }) => {
-      if (isServer) {
-        const antStyles = /antd\/.*?\/style.*?/;
-        const origExternals = [...config.externals];
-        config.externals = [
-          (context, request, callback) => {
-            if (request.match(antStyles)) return callback();
-            if (typeof origExternals[0] === 'function') {
-              origExternals[0](context, request, callback);
-            } else {
-              callback();
-            }
-          },
-          ...(typeof origExternals[0] === 'function' ? [] : origExternals),
-        ];
-
-        config.module.rules.unshift({
-          test: antStyles,
-          use: 'null-loader',
-        });
-      }
-
-      config.module.rules.push(
-        {
-          test: /\.(png|svg|eot|otf|ttf|woff|woff2)$/,
-          use: {
-            loader: 'url-loader',
-            options: {
-              limit: 8192,
-              publicPath: '/_next/static/',
-              outputPath: 'static/',
-              name: '[name].[ext]',
+        config.module.rules.push(
+          {
+            test: /\.(png|svg|eot|otf|ttf|woff|woff2)$/,
+            use: {
+              loader: 'url-loader',
+              options: {
+                limit: 8192,
+                publicPath: '/_next/static/',
+                outputPath: 'static/',
+                name: '[name].[ext]',
+              },
             },
           },
-        },
-        {
-          test: /\.gql$/,
-          exclude: /node_modules/,
-          loader: 'graphql-tag/loader',
-        }
-      );
+          {
+            test: /\.gql$/,
+            exclude: /node_modules/,
+            loader: 'graphql-tag/loader',
+          }
+        );
 
-      return config;
-    },
-  })
+        return config;
+      },
+    })
+  )
 );
