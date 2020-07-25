7971f21a954b34ddb364ce0fc440578122bb0a63
Michael Donahue <mtdonahue1@gmail.com>
2020-07-25 00:26:12 -0700

    refactor: auth views into partials
    - Allows partials to be used on auth pages and modal
    - Allows for swapping views or route change

--- src/components/app/modal/components/header/header.tsx
+++ src/components/app/modal/components/header/header.tsx
@@ -1,15 +1,13 @@
 import React from 'react';
+import cx from 'classnames';
 import { ModalContext } from '../../modal-context';
+import styles from './header.module.scss';
 
-const Header = ({ children }: any) => {
+const Header = ({ className, children }: any) => {
   return (
     <ModalContext.Consumer>
       {() => {
-        return (
-          <div>
-            <div>{children}</div>
-          </div>
-        );
+        return <div className={cx(styles.header, className)}>{children}</div>;
       }}
     </ModalContext.Consumer>
   );
