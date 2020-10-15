a196c3fec68270cb8926f59b934d0016ad78a2a5
Michael Donahue <mtdonahue1@gmail.com>
2020-10-14 23:13:14 -0700

    feat: drawer component
    - And put shopping cart inside a drawer

--- src/views/app/products/products.tsx
+++ src/views/app/products/products.tsx
@@ -1,87 +1,46 @@
 import React from 'react';
-import Link from 'next/link';
 import { useGetProducts } from '@modules/queries/products';
 import { useShoppingCart } from '@components/hooks/use-shopping-cart';
-import { ShoppingCart, Dropdown, Button } from '@components/app';
+import { Button } from '@components/app';
+import { ShoppingCart } from '@features/shopping-cart';
 // import styles from './products.module.scss';
 
 const Products = () => {
-  const { items, total, addCartItem } = useShoppingCart();
+  const { addCartItem } = useShoppingCart();
   const { data: response, isLoading } = useGetProducts();
   const products = response?.data;
 
   return (
-    <div className="my-4 mx-8">
-      <div className="flex justify-end mb-4">
-        <Dropdown alignEnd>
-          <Dropdown.Toggle id="shopping-cart">
-            <ShoppingCart count={items.length} />
-          </Dropdown.Toggle>
+    <div style={{ height: 2000 }}>
+      <div className="my-4 mx-8">
+        <div className="flex justify-end mb-4">
+          <ShoppingCart />
+        </div>
+        <div className="grid grid-cols-4 gap-4">
+          {!isLoading &&
+            products?.map((result) => {
+              const productPrice =
+                (result.relationships?.price.unit_amount || 0) / 100;
 
-          <Dropdown.Menu
-            role="list"
-            className="p-2 shadow-md bg-white space-y-2 rounded-sm"
-          >
-            <ul>
-              {items?.map((item) => {
-                return (
-                  <li key={item.attributes.id}>
-                    <div className="flex justify-between space-x-4">
-                      <div className="whitespace-no-wrap">
-                        {item.attributes.name}
-                      </div>
-                      <div>
-                        {(
-                          (item.relationships?.price.unit_amount || 0) / 100
-                        ).toLocaleString('en-US', {
-                          style: 'currency',
-                          currency: 'USD',
-                        })}
-                      </div>
-                    </div>
-                  </li>
-                );
-              })}
-
-              <li className="text-right">
-                Total:{' '}
-                {total.toLocaleString('en-US', {
-                  style: 'currency',
-                  currency: 'USD',
-                })}
-              </li>
-            </ul>
-
-            <Link href="/app/cart" as="/app/cart">
-              <Button>Go to Cart</Button>
-            </Link>
-          </Dropdown.Menu>
-        </Dropdown>
-      </div>
-      <div className="grid grid-cols-4 gap-4">
-        {!isLoading &&
-          products?.map((result) => {
-            const productPrice =
-              (result.relationships?.price.unit_amount || 0) / 100;
-
-            return (
-              <div key={result.attributes.id}>
-                <img
-                  src={`/images/products/${result.attributes.id}.jpg`}
-                  alt={result.attributes.name}
-                  width="100%"
-                />
-                <div>{result.attributes.name}</div>
-                <div>
-                  {productPrice.toLocaleString('en-US', {
-                    style: 'currency',
-                    currency: 'USD',
-                  })}
+              return (
+                <div key={result.attributes.id}>
+                  <img
+                    src={`/images/products/${result.attributes.id}.jpg`}
+                    alt={result.attributes.name}
+                    width="100%"
+                  />
+                  <div>{result.attributes.name}</div>
+                  <div>
+                    {productPrice.toLocaleString('en-US', {
+                      style: 'currency',
+                      currency: 'USD',
+                    })}
+                  </div>
+                  <Button onClick={() => addCartItem(result)}>Buy Now</Button>
                 </div>
-                <Button onClick={() => addCartItem(result)}>Buy Now</Button>
-              </div>
-            );
-          })}
+              );
+            })}
+        </div>
       </div>
     </div>
   );
