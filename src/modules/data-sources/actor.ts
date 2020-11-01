b4e73a3833c190307341793659d8cf0823d90af0
Michael Donahue <mtdonahue1@gmail.com>
2020-10-31 22:27:53 -0700

    chore: clean up unused routes

--- src/modules/data-sources/actor.ts
+++
@@ -1,8 +0,0 @@
-import { request } from '@modules/request';
-
-const getActorCartItems = async (actorId: string) => {
-  const response = await request.get(`/api/v1/actors/${actorId}/cart-items`);
-  return response.data;
-};
-
-export { getActorCartItems };
--- src/modules/data-sources/carts.ts
+++ src/modules/data-sources/carts.ts
@@ -1,9 +1,8 @@
 import { AxiosResponse } from 'axios';
 import { request } from '@modules/request';
 import { Data } from '@modules/api-response';
-import { Product, CartProduct } from '@typings/entities/product';
-import { Cart } from '@typings/entities/cart';
-import { CartItem } from '@typings/entities/cart-item';
+import { GetCart, Cart } from '@typings/entities/cart';
+import { CartProduct } from '@typings/entities/product';
 
 export type SyncCartItems = {
   userId: string;
@@ -14,16 +13,14 @@ export type ChangeCartStatus = {
   status: string;
 };
 
-const getMyCart = async (): Promise<
-  AxiosResponse<
-    Data<Partial<Cart>, { cart_items: CartItem[]; products: Product[] }>
-  >
-> => {
+const getMyCart = async (): Promise<AxiosResponse<GetCart>> => {
   const response = await request.get('/api/v1/carts/me');
   return response.data;
 };
 
-const createCart = async (body: any = {}) => {
+const createCart = async (
+  body: any = {}
+): Promise<AxiosResponse<Data<Partial<Cart>>>> => {
   const response = await request.post('/api/v1/carts', { ...body });
   return response.data;
 };
